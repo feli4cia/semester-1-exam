0dc7871e45cf7808a0ef5fc097dfe733c584fdc7
FeliciaCimpoaca <>
2021-11-16 10:50:43 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,74 @@
+
+#### Describe each datatype below:(4 marks)
+
+## 4         = INTEGER 
+## 5.7       =FLOAT
+## True      = BOOLEAN
+## Good Luck = STRING 
+
+#### Which datatype would be useful for storing someone's height? (1 mark)
+## Answer : float
+
+#### Which datatype would be useful for storing someone's hair colour?(1 mark)
+## Answer: string
+
+
+####Create a variable tha will store the users name.(1 mark)
+#name=input("please enter your name")
+
+
+####Create a print statement that will print the first 4 characters of the person's name.(3 marks)
+
+#print(name[0:6])
+
+
+####Convert the user's name to all uppercase characters and print the result
+
+## string_name = "This in a phynton string"
+
+####Find out how many times the letter A occurs in the user's name
+
+#counter = 0
+
+
+
+
+
+#### Create a conditional statement to ask a user to enter their age. If they are older than 18 they receive a message saying they can enter the competition, if they are under 18, they receive a message saying they cannot enter 
+
+#age = int(input("Enter your age:"))
+
+#if age >= 18:
+#print("You cant enter the competition")
+#elif age <= 18:
+#print : "you cant enter the competition"
+
+
+
+#### Create an empty list called squareNumbers (3 marks) 
+
+#squareNumbersList=[]
+
+
+
+
+
+
+
+#### Square numbers are the solutions to a number being multiplied by itself( example 1 is a square number because 1 X 1 = 1, 4 is a square number because 2 X 2 = 4 ). 
+###Calculate the first 20 square numbers and put them in the list called squareNumbers. (With loop and .append 10 marks, without, Max 6 marks).
+
+
+
+
+####Print your list (1 mark)
+
+####Create a variable called userSquare that asks the user for their favourite square number
+
+
+#### Add this variable to the end of your list called SquareNumbers
+
+### Create a variable called choice. This variable should choose a random element from your list. Print the variable called choice.(3 marks)
+
+
+####Create another print statement that prints tha following output: The random square number is: XX, where XX is where the random square number chosen by the computer.(4 marks)
